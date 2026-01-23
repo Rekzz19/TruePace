256fe94cb0bff1815777cc54993c02592d423b29
kolawole afonja <k1@kolawoles-Air.lan>
2026-01-23 21:32:22 +0000

    latest/onbording

--- src/app/onboarding/steps/step-two/schema.ts
+++ src/app/onboarding/steps/step-two/schema.ts
@@ -1,22 +1,28 @@
 import { z } from "zod";
 
 export const stepTwoSchema = z.object({
-    experienceLevel: z.enum(["BEGINNER", "INTERMEDIATE", "ADVANCED", "PROFESSIONAL"]),
-    runningGoal: z.enum(["TWO-KM", "FIVE-KM", "TEN-KM", "MARATHON"]),
-    availability: z.array(z.enum([
-        "monday",
-        "tuesday",
-        "wednesday",
-        "thursday",
-        "friday",
-        "saturday",
-        "sunday",
-      ])
-    ).min(1, "Select at least one day"),
-    injuryHistory: z.string().min(1),
+  experienceLevel: z.enum(["BEGINNER", "INTERMEDIATE", "ADVANCED", "PROFESSIONAL"],{
+    message: "Please select your experience level"
+  }),
+  runningGoal: z.enum(["TWO-KM", "FIVE-KM", "TEN-KM", "MARATHON"],{
+    message: "Please select your running goal"
+  }),
+  availability: z.array(z.enum([
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+      "sunday",
+    ])
+  ).min(1, "Select at least one day"),
+  injuryHistory: z.string(),
 
 });
 
 export type stepTwoValues = z.infer<typeof stepTwoSchema>
 
 export type stepTwoInput = z.input<typeof stepTwoSchema>
+
+//      
